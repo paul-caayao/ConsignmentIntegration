78aa8c55022666a095f18498b769d6242292a39e
paul-caayao <paulcaayao@gmail.com>
2024-11-06 11:21:19 +0800

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using ConsignmentIntegration.Models;
+using Microsoft.Extensions.Configuration;
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+var ftpSettings = configuration.GetSection("FtpSettings").Get<FtpSettings>();
+
+Console.WriteLine("Consignment Integration - Transvirtual Technical Test by Paul Caayao");
+Console.WriteLine($"Connecting to FTP Server {ftpSettings.Server}:{ftpSettings.Port} to access XML file...");
+
